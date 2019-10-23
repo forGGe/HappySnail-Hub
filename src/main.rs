d8323a84a2b476ad7224a17cedaebaa8aa6b5074
Max Payne <forgge@gmail.com>
2019-10-23 20:29:29 +0300

    Stub for ModBus master requests

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,134 @@ extern crate clap;
 extern crate paho_mqtt as mqtt;
 
 use std::process;
+use std::vec;
 use std::time::Duration;
+use std::num::Wrapping;
 use clap::{Arg, App};
+
+trait RequestPDU {
+    const FN_ID: u8;
+    fn as_ascii(&self) -> String;
+    fn lrc(&self, slave: u8) -> u8;
+}
+
+fn generic_lrc(data: &[u8]) -> u8 {
+    // ISO 1155
+    // u16 helps to avoid using std::Wrapping
+
+    let r = data.iter().fold(0u16, |acc, x| (acc + *x as u16) & 0xff);
+    (((r ^ 0xff) + 1) & 0xff) as u8
+}
+
+//-----
+
+struct RequestPair(u16, u16);
+
+impl RequestPair {
+    fn lrc(&self, slave: u8, fn_id: u8) -> u8 {
+        generic_lrc(
+            &[
+                slave,
+                fn_id,
+                (self.0 >> 8) as u8, self.0 as u8,
+                (self.1 >> 8) as u8, self.1 as u8
+            ]
+        )
+    }
+
+    fn as_ascii(&self, fn_id: u8) -> String {
+        format!("{:02X}{:04X}{:04X}", fn_id, self.0, self.1)
+    }
+}
+
+//-----
+
+struct ReadCoils {
+    addr: u16,
+    cnt:  u16,
+}
+
+impl RequestPDU for ReadCoils {
+    const FN_ID: u8 = 0x01;
+
+    fn as_ascii(&self) -> String {
+        RequestPair(self.addr, self.cnt).as_ascii(Self::FN_ID)
+    }
+
+    fn lrc(&self, slave: u8) -> u8 {
+        RequestPair(self.addr, self.cnt).lrc(slave, Self::FN_ID)
+    }
+}
+
+//-----
+
+struct ReadDiscreteInputs {
+    addr: u16,
+    cnt:  u16,
+}
+
+impl RequestPDU for ReadDiscreteInputs {
+    const FN_ID: u8 = 0x02;
+
+    fn as_ascii(&self) -> String {
+        RequestPair(self.addr, self.cnt).as_ascii(Self::FN_ID)
+    }
+
+    fn lrc(&self, slave: u8) -> u8 {
+        RequestPair(self.addr, self.cnt).lrc(slave, Self::FN_ID)
+    }
+}
+
+//-----
+
+struct ReadHoldingRegisters {
+    addr: u16,
+    cnt:  u16,
+}
+
+impl RequestPDU for ReadHoldingRegisters {
+    const FN_ID: u8 = 0x03;
+
+    fn as_ascii(&self) -> String {
+        RequestPair(self.addr, self.cnt).as_ascii(Self::FN_ID)
+    }
+
+    fn lrc(&self, slave: u8) -> u8 {
+        RequestPair(self.addr, self.cnt).lrc(slave, Self::FN_ID)
+    }
+}
+
+//-----
+
+struct WriteSingleCoil {
+    addr: u16,
+    coil: bool,
+}
+
+impl RequestPDU for WriteSingleCoil {
+    const FN_ID: u8 = 0x05;
+
+    fn as_ascii(&self) -> String {
+        RequestPair(self.addr, if self.coil { 0xff00 } else { 0x0000 }).as_ascii(Self::FN_ID)
+    }
+
+    fn lrc(&self, slave: u8) -> u8 {
+        RequestPair(self.addr, if self.coil { 0xff00 } else { 0x0000 }).lrc(slave, Self::FN_ID)
+    }
+}
+
+//-----
+
+fn make_request_frame(slave_address: u8, pdu: impl RequestPDU) -> String {
+    format!(":{:02X}{}{:02X}\r\n", slave_address, pdu.as_ascii(), pdu.lrc(slave_address))
+}
+
+//-----
 
 fn main() {
     println!("Hello, world!");
+
+    println!("{}", make_request_frame(247, ReadHoldingRegisters { addr: 5001, cnt: 10 }));
 
     let matches = App::new("HappySnail Hub")
         .version("0.1.0")
